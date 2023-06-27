20c81ccc14d923773f173093a3756db7be06b294
victorlmafra <104863916+victorlmafra@users.noreply.github.com>
2023-06-26 22:33:19 -0300

    Add files via upload

---
+++ run.java
@@ -0,0 +1,17 @@
+package main;
+
+import javax.swing.SwingUtilities;
+
+import interfaces.inicio;
+
+public class rodar {
+    public static void main(String[] args) {
+        OptionsScreen();
+
+    }
+
+    public static void OptionsScreen() {
+
+    }
+
+}
